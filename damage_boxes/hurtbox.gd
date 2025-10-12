46c4ed07965825440eb5212e1785bf1feac7e9fe
jonatay <jono.stretch@gmail.com>
2025-10-12 19:46:40 +0200

    AI Chat Checkpoint: msg_0_2025-10-12T19-46-40 - how to generate sprite image in orca

--- damage_boxes/hurtbox.gd
+++ damage_boxes/hurtbox.gd
@@ -6,8 +6,8 @@ signal hurt(hitbox:Hitbox)
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
-func _on_area_entered(area: Area2D) -> void:
-	if area is not Hitbox:
+func _on_area_entered(other_area: Area2D) -> void:
+	if other_area is not Hitbox:
 		return
-	# emit_signal("hurt", area as Hitbox)
-	hurt.emit(area as Hitbox)
+	# emit_signal("hurt", other_area as Hitbox)
+	hurt.emit(other_area as Hitbox)
